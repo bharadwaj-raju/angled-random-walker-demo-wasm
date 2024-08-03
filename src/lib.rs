10d2ed84ff6df334074430935bb0b1fa7c641934
Bharadwaj Raju <bharadwaj.raju777@gmail.com>
2024-08-03 15:43:33 +0530

    remove Paint enum

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,6 @@
 use angled_random_walker as arw;
-use getrandom::getrandom;
-use stackblur_iter::{
-    blur,
-    imgref::{Img, ImgExtMut, ImgRefMut},
-};
+use stackblur_iter::{blur, imgref::Img};
 use wasm_bindgen::prelude::*;
-
-#[wasm_bindgen]
-pub enum Paint {
-    Age,
-    CumulativeAge,
-    Generation,
-    Constant,
-}
 
 fn scale_age_to_height(age: f64, max_age: f64) -> f64 {
     age / max_age
@@ -30,12 +18,9 @@ pub fn generate(
     max_long_angle_divergence: f64,
     max_short_angle_divergence: f64,
     short_branch_frequency: usize,
-    paint: Paint,
+    seed_bytes: &[u8],
 ) -> Vec<u8> {
-    let mut seed_byes = [0u8; 8];
-    if getrandom(&mut seed_byes).is_err() {
-        seed_byes = [1, 2, 3, 4, 5, 6, 7, 8];
-    }
+    let seed_bytes: [u8; 8] = seed_bytes.try_into().unwrap_or([1, 2, 3, 4, 5, 6, 7, 8]);
     let params = arw::SimulationParams {
         size,
         max_long_age,
@@ -45,14 +30,9 @@ pub fn generate(
         max_long_angle_divergence,
         max_short_angle_divergence,
         short_branch_frequency,
-        paint: match paint {
-            Paint::Age => arw::Paint::Age,
-            Paint::CumulativeAge => arw::Paint::CumulativeAge,
-            Paint::Generation => arw::Paint::Generation,
-            Paint::Constant => arw::Paint::Constant,
-        },
+        paint: arw::Paint::CumulativeAge,
         initial_walkers: arw::InitialWalkers::CardinalsAndOrdinals,
-        seed: u64::from_le_bytes(seed_byes),
+        seed: u64::from_le_bytes(seed_bytes),
     };
     let max_age = max_long_age * (max_generations + 1) + max_generations + 1;
     arw::simulate(params)
@@ -83,10 +63,21 @@ pub fn to_image(data: Vec<u8>) -> Vec<u8> {
 }
 
 #[wasm_bindgen]
-pub fn heightmap_blur(data: Vec<u8>, radius: usize) -> Vec<u8> {
-    let mut img = Img::new(data, 512, 512);
+pub fn heightmap_blur(data: Vec<u8>, radius: usize, detail_max: u8) -> Vec<u8> {
+    let mut img = Img::new(data.clone(), 512, 512);
     blur(&mut img.as_mut(), radius, |p| *p as usize, |p| p as u8);
-    img.buf().clone()
+    let mut detail_img = Img::new(
+        data.iter().map(|d| (*d).clamp(0, detail_max)).collect(),
+        512,
+        512,
+    );
+    blur(&mut detail_img.as_mut(), 4, |p| *p as usize, |p| p as u8);
+    let first_stage = img.buf().clone();
+    first_stage
+        .iter()
+        .zip(detail_img.buf())
+        .map(|(blurred_val, raw_data)| blurred_val + raw_data)
+        .collect()
 }
 
 #[wasm_bindgen]
